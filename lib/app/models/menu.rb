07b672786732ae15bd8b25e975a9b562e6dc4a33
quadroturkey <reesemb1@live.com>
2020-02-11 11:13:56 -0600

    started splitting up run methods

--- lib/app/models/menu.rb
+++ lib/app/models/menu.rb
@@ -1,26 +1,26 @@
+
 class Menu
   attr_accessor :user
 
   def run
-      welcome
+    welcome
   end
 
   def welcome
-      puts "Welcome to the game"
-      puts "Do you have an existing account? (Y/N)"
+    puts "Welcome to the game"
+    puts "Do you have an existing account? (Y/N)"
 
-      input = gets.chomp
+    input = gets.chomp
 
-      case input.downcase
-      when "y" || "Y"
-          login
-      when "n" || "N"
-          create_new_user
-      else
-          puts "Invalid Entry. Type Y for yes or N for no."
-          welcome
-      end
-
+    case input.downcase
+    when "y" || "Y"
+      login
+    when "n" || "N"
+      create_new_user
+    else
+      puts "Invalid Entry. Type Y for yes or N for no."
+      welcome
+    end
   end
 
   def login
@@ -35,10 +35,10 @@ class Menu
       homescreen(User.create(name: input))
   end
 
-  def display_account_info(user)
-      puts "Username: #{user.name}".colorize(:blue)
-      puts "Balance : #{user.balance}".colorize(:green)
-      homescreen(user)
+  def display_account_info
+      puts "Username: #{self.user.name}".colorize(:blue)
+      puts "Balance : #{self.user.balance}".colorize(:green)
+      homescreen
   end
 
   def display_upcoming_games(game_index)
@@ -73,25 +73,27 @@ class Menu
 
   end
 
-  def homescreen(user)
-      puts "Welcome, #{user.name}"
-      puts "Please select an option:\n1. Account Info\n2. Upcoming Games\n3. Current Bets\n4. Bet History"
+  def homescreen(user = self.user)
+    self.user = user
+    puts "Welcome, #{user.name}"
+    puts "Please select an option:\n1. Account Info\n2. Upcoming Games\n3. Current Bets\n4. Bet History\n5. EXIT\n"
 
-      input = gets.chomp.to_i
+    input = gets.chomp.to_i
 
-      case input
-      when 1
-          display_account_info(user)
-      when 2
-          display_upcoming_games(0)
-      when 3
-          display_current_bets(user)
-      when 4
-          display_all_bets(user)
-      else
-          puts "Invalid Entry."
-          homescreen(user)
-      end
-
+    case input
+    when 1
+      display_account_info
+    when 2
+      display_upcoming_games(0)
+    when 3
+      display_current_bets(user)
+    when 4
+      display_all_bets(user)
+    when 5
+      puts "Goodbye"
+    else
+      puts "Invalid Entry."
+      homescreen(user)
+    end
   end
 end
